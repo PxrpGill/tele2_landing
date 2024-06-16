10046bf91cc3eda73e6bbe0533a47c11aeb3c0b9
Eugene <jhongil3007@gmail.com>
2024-06-16 20:46:40 +0300

    шаблонизация

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { browserslistToTargets } from "lightningcss";
+import browserslist from "browserslist";
+import handlebars from "vite-plugin-handlebars";
+
+
+const components = 'src/components';
+
 
 export default defineConfig({
     base: "/tele2_landing/",
     root: "src/",
     publicDir: resolve(__dirname, 'public'),
+    css: {
+        transformer: 'lightningcss',
+        lightningcss: {
+            targets: browserslistToTargets(browserslist('>= 0.25%'))
+        }
+    },
     build: {
         outDir: "../dist",
         minify: true,
+        cssMinify: 'lightningcss',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src/index.html')
@@ -21,4 +35,15 @@ export default defineConfig({
             '/components': resolve(__dirname, 'src/components')
         }
     },
+    plugins: [
+        handlebars({
+            partialDirectory: [
+                resolve(__dirname, `${components}/header`),
+                resolve(__dirname, `${components}/main`),
+                resolve(__dirname, `${components}/footer`),
+                resolve(__dirname, `${components}/dialog_question`),
+                resolve(__dirname, `${components}/dialog_change`)
+            ]
+        })
+    ]
 });
