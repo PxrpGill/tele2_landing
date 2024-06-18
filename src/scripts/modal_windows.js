0f0f4e1317c422ba43a2def7439c01b32fd12291
Eugene <128057676+PxrpGill@users.noreply.github.com>
2024-06-18 14:33:12 +0300

    модальное окно второе

--- src/scripts/modal_windows.js
+++ src/scripts/modal_windows.js
@@ -1,9 +1,6 @@
 const header = document.querySelector('.header__geoposition');
 const geopositionButton = document.querySelector('.header__geoposition-button');
 const mainContainer = document.querySelector('.main__container');
-let statePage = {
-    region: 'Санкт Петербург'
-}
 
 const templateQuestionNode = document.querySelector('.choose-region-question');
 const modalTemplateNodeQuestion = templateQuestionNode.content.cloneNode(true);
@@ -11,43 +8,59 @@ const modalWindow = modalTemplateNodeQuestion.querySelector('.choose-region-ques
 const agreeButton = modalWindow.querySelector('.choose-region-question__agree-button');
 const changeRegionShowButton = modalWindow.querySelector('.choose-region-question__change-city-button');
 
-
 const dialogTimeId = setTimeout(() => {
-    if (!statePage.regionQuestion) {
-        header.appendChild(modalTemplateNodeQuestion);
-        header.style.border = 'none';
-        document.body.style.overflow = 'hidden';
-        statePage.regionQuestion = !statePage.regionQuestion;
-        modalWindow.showModal();
-    }
+    const modalContainer = document.createElement('div');
+    modalContainer.className = 'header__modal-container';
+
+    modalContainer.appendChild(modalTemplateNodeQuestion);
+    header.appendChild(modalContainer);
+
+    header.style.border = 'none';
+    document.body.style.overflow = 'hidden';
+
+    modalWindow.showModal();
 }, 100);
 
 agreeButton.addEventListener('click', () => {
     document.body.style.overflow = '';
     modalWindow.close();
-    header.removeChild(modalTemplateNodeQuestion);
+    
+    const modalContainer = header.querySelector('.header__modal-container');
+    header.removeChild(modalContainer);
 });
-
 
 const templateChangeNode = document.querySelector('.change-region');
 const modalTemplateNodeChange = templateChangeNode.content.cloneNode(true);
 const modalWindowChange = modalTemplateNodeChange.querySelector('.change-region__modal-window');
 const regionButtons = modalTemplateNodeChange.querySelectorAll('.change-region__item-button');
 
-
 changeRegionShowButton.addEventListener('click', () => {
-    document.body.style.overflow = '';
+    const modalContainer = header.querySelector('.header__modal-container');
     modalWindow.close();
-    if (header.contains(modalTemplateNodeQuestion)) {
-        header.removeChild(modalTemplateNodeQuestion);
-    }
+    header.removeChild(modalContainer);
+
+    const modalContainerChange = document.createElement('div');
+    modalContainerChange.className = 'main__modal-container';
+    modalContainerChange.appendChild(modalTemplateNodeChange);
+    mainContainer.appendChild(modalContainerChange);
+    modalWindowChange.showModal();
+});
+
+geopositionButton.addEventListener('click', () => {
+    const modalContainerChange = document.createElement('div');
+    modalContainerChange.className = 'main__modal-container';
+    modalContainerChange.appendChild(modalTemplateNodeChange);
+    mainContainer.appendChild(modalContainerChange);
+    modalWindowChange.showModal();
 });
 
 regionButtons.forEach(button => {
     button.addEventListener('click', () => {
         geopositionButton.textContent = button.textContent;
         document.body.style.overflow = '';
+        
+        const modalContainerChange = mainContainer.querySelector(".main__modal-container");
         modalWindowChange.close();
-        mainContainer.removeChild(modalTemplateNodeChange);
+        mainContainer.removeChild(modalContainerChange);
     });
 });
