fda58969155ef8d367d7564857a9aec3f99ed174
Eugene <jhongil3007@gmail.com>
2024-06-24 12:51:06 +0300

    подправлены стили

--- src/scripts/validate_input.js
+++ src/scripts/validate_input.js
@@ -1,6 +1,6 @@
 export default class Validate {
     constructor() {
-        this.timeLiveMessage = 3000;
+        this.timeLiveMessage = 300000;
         this.successTemplate = document.querySelector('.main__success-message-template');
         this.failedTemplate = document.querySelector('.main__failed-message-template');
         this.failedMessagePhoneRetry = 'На этот номер уже выслан промокод';
