c2dd5a0465b50ac55b4074d08ee324eb93c388d5
Eugene <jhongil3007@gmail.com>
2024-06-25 15:35:01 +0300

    вынос обнуление стилей в отедельный css файл

--- src/scripts/lightning.js
+++ src/scripts/lightning.js
@@ -3,35 +3,39 @@ import fs from 'fs';
 
 let mixins = new Map();
 
-let cssCode = fs.readFileSync('src/styles/style.css');
+const files = {
+    "header.css": fs.readFileSync('src/styles/header.css'),
+    "mixins_position.css": fs.readFileSync('src/styles/mixins_position.css')
+}
 
-let res = transform({
-    filename: 'style.css',
-    minify: true,
-    code: cssCode,
-    customAtRules: {
-        mixin: {
-            prelude: '<custom-ident>',
-            body: 'style-block'
+for (let key in files) {
+    let res = transform({
+        filename: 'style.css',
+        minify: true,
+        code: files[key],
+        customAtRules: {
+            mixin: {
+                prelude: '<custom-ident>',
+                body: 'style-block'
+            },
+            apply: {
+                prelude: '<custom-ident>'
+            }
         },
-        apply: {
-            prelude: '<custom-ident>'
-        }
-    },
-    visitor: {
-        Rule: {
-            custom: {
-                mixin(rule) {
-                    mixins.set(rule.prelude.value, rule.body.value);
-                    return [];
-                },
-                apply(rule) {
-                    return mixins.get(rule.prelude.value);
+        visitor: {
+            Rule: {
+                custom: {
+                    mixin(rule) {
+                        mixins.set(rule.prelude.value, rule.body.value);
+                        return [];
+                    },
+                    apply(rule) {
+                        return mixins.get(rule.prelude.value);
+                    }
                 }
             }
         }
-    }
-});
+    });
 
-fs.writeFileSync('src/styles/transformed_style.css', res.code.toString());
-
+    fs.writeFileSync(`src/styles/${key}`, res.code.toString());
+}
