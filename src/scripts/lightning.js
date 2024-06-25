64636dff0a0479313d9fd42958a2def122a74fe8
Eugene <jhongil3007@gmail.com>
2024-06-26 01:30:21 +0300

    работающие миксины для header, footer, main, первой и последней секции

--- src/scripts/lightning.js
+++ src/scripts/lightning.js
@@ -5,68 +5,79 @@ import fs from 'fs';
 let mixins = new Map();
 
 const baseUrl = 'src/styles/unformatted';
+const mixinsFile = `mixins.css`;
 
 const files = [
     "style.css",
     "header.css",
     "main.css",
     "footer.css",
-
     "sections/rate.css",
     "sections/slider.css",
     "sections/stocks.css",
     "sections/tariff.css",
-
     "modal/change_region.css",
     "modal/participate.css"
 ];
 
-async function processFiles() {
-    for (let key of files) {
-        const filePath = path.join(baseUrl, key);
+async function processFile(key, collectMixins = false) {
+    const filePath = path.join(baseUrl, key);
 
-        const { code: buffer } = await bundleAsync({
-            filename: filePath,
-            minify: true,
-            resolver: {
-                read(filePath) {
-                    return fs.readFileSync(filePath, 'utf8');
-                },
-                resolve(specifier, from) {
-                    return path.resolve(path.dirname(from), specifier);
-                }
-            }
-        });
-
-        let res = transform({
-            filename: key,
-            minify: true,
-            code: Buffer.from(buffer),
-            customAtRules: {
-                mixin: {
-                    prelude: '<custom-ident>',
-                    body: 'style-block'
-                },
-                apply: {
-                    prelude: '<custom-ident>'
-                }
+    const { code: buffer } = await bundleAsync({
+        filename: filePath,
+        minify: true,
+        resolver: {
+            read(filePath) {
+                return fs.readFileSync(filePath, 'utf8');
             },
-            visitor: {
-                Rule: {
-                    custom: {
-                        mixin(rule) {
+            resolve(specifier, from) {
+                return path.resolve(path.dirname(from), specifier);
+            }
+        }
+    });
+
+    let res = transform({
+        filename: key,
+        minify: true,
+        code: Buffer.from(buffer),
+        customAtRules: {
+            mixin: {
+                prelude: '<custom-ident>',
+                body: 'style-block'
+            },
+            apply: {
+                prelude: '<custom-ident>'
+            }
+        },
+        visitor: {
+            Rule: {
+                custom: {
+                    mixin(rule) {
+                        if (collectMixins) {
                             mixins.set(rule.prelude.value, rule.body.value);
                             return [];
-                        },
-                        apply(rule) {
-                            return mixins.get(rule.prelude.value);
                         }
+                        return null;
+                    },
+                    apply(rule) {
+                        return mixins.get(rule.prelude.value);
                     }
                 }
             }
-        });
+        }
+    });
+
+    if (!collectMixins) {
         fs.writeFileSync(`src/styles/formatted/${key}`, res.code.toString());
     }
 }
 
-processFiles();
+async function processFiles() {
+    await processFile(mixinsFile, true);
+
+    for (let key of files) {
+        await processFile(key);
+    }
+}
+
+processFiles().catch(console.error);
