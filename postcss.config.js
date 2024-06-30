91c04826e1a9115840e9da14e62e826f17f456dd
Eugene <jhongil3007@gmail.com>
2024-06-30 18:32:14 +0300

    подключение Lightningcss и корректировка использования миксина

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ import postcssSimpleVars from 'postcss-simple-vars';
 import postcssCalc from 'postcss-calc';
 import autoprefixer from 'autoprefixer';
 import postcssImport from 'postcss-import';
+import lightningcssPlugin from 'postcss-lightningcss';
 
 
 export default {
@@ -12,5 +13,15 @@ export default {
         postcssCalc(),
         autoprefixer(),
         postcssImport(),
+        lightningcssPlugin({
+            browsers: '>= .25%',
+            lightningcssOptions: {
+                minify: true,
+                sourceMap: true,
+                targets: {
+                    safari: (13 << 16) | (2 << 8),
+                },
+            }
+        })
     ]
 };
--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
     build: {
         outDir: "../dist",
         minify: true,
-        cssMinify: 'lightningcss',
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src/index.html')
